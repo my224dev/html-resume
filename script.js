bf5544917ca372cdc03416c814f6ca97e327fb50
FacinetKouyate <facinetkouyatem14@gmail.com>
2021-12-02 01:47:06 +0200

    Html element editable

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
+
 (function(){
-    autoRefresh(5000);
+    autoRefresh(36000);
 })();
 
 /** this function refresh the page automaticaly every 5 sec */
 function autoRefresh(timeMs) {
     setTimeout("location.reload(true)", timeMs);
+}
+
+document.getElementById("btn_name").addEventListener("click", function(){
+    changeName();
+});
+
+function changeName() {
+
+  const newName = prompt("new name");
+  (document.querySelector("#full_name")).textContent = newName;
+    
 }
